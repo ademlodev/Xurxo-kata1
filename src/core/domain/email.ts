e6608c88b62d02f274f14ad1ecaa4d0a12429518
Javier García <javier.garcia.olmeda@gmail.com>
2023-11-14 15:17:57 +0100

    format with prettier

--- src/core/domain/email.ts
+++ src/core/domain/email.ts
@@ -1,13 +1,16 @@
-export class Email { //TODO implements ValueObject {
-    value: string;
+export class Email {
+    //TODO implements ValueObject {
+    value: string
 
     constructor(email: string) {
-        this.value = email;
+        this.value = email
     }
 
     public validate(): boolean {
-        const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        const regexp = new RegExp(
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
 
-        return regexp.test(this.value);
+        return regexp.test(this.value)
     }
 }
--- test/core/email.test.ts
+++ test/core/email.test.ts
@@ -1,24 +1,23 @@
-import { beforeEach, describe } from "node:test";
-import { Email } from "../../src/core/domain/email";
+import { beforeEach, describe } from 'node:test'
+import { Email } from '../../src/core/domain/email'
 
-describe('Email Tests', ()=>{
+describe('Email Tests', () => {
+    const EXAMPLE_EMAIL = 'example@example.com'
+    let sut: Email
 
-  const EXAMPLE_EMAIL = 'example@example.com';
-  let sut: Email;
-
-  beforeEach(() => {
-    sut = new Email(EXAMPLE_EMAIL);
-  });
-
-  describe('Create Email', () =>{
-    it('should create an instance of Email', ()=> {
-      expect(sut).toBeInstanceOf(Email);
-    });
-  })
-
-  describe('Email Validations', () => {
-    it('should be valid email address', () => {
-      expect(sut.validate()).toBeTruthy();
+    beforeEach(() => {
+        sut = new Email(EXAMPLE_EMAIL)
     })
-  })
-});
+
+    describe('Create Email', () => {
+        it('should create an instance of Email', () => {
+            expect(sut).toBeInstanceOf(Email)
+        })
+    })
+
+    describe('Email Validations', () => {
+        it('should be valid email address', () => {
+            expect(sut.validate()).toBeTruthy()
+        })
+    })
+})
