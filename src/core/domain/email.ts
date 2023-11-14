1be4aae554314cf712f0f316a0e465421408efff
Javier García <javier.garcia.olmeda@gmail.com>
2023-11-14 10:30:46 +0100

    config with ts, eslint and prettier

--- src/core/domain/email.ts
+++ src/core/domain/email.ts
@@ -6,7 +6,7 @@ export class Email implements ValueObject {
     }
 
     public validate(): boolean {
-        const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        const regexp = new RegExp(/'/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/');
 
         return this.value.match(regexp);
     }
