e8b990d5f4d9da2167b21cb6019da8c33ebeef8d
Judit Sánchez <judit@jelly-brains.com>
2023-11-14 15:19:59 +0100

    feat(user): create basic user class

--- test/core/password.test.ts
+++ test/core/password.test.ts
@@ -3,23 +3,29 @@ import { Password } from "../../src/core/domain/password";
 
 describe('Password', ()=>{
 
-  const EXAMPLE_PASSWORD = 'pass';
-  let pass: any;
+  const EXAMPLE_PASSWORD_WRONG = 'pass';const EXAMPLE_PASSWORD_OK = 'pass123word';
+  let passWrong: any;
+  let passOK: any;
 
   beforeEach(() => {
-    pass = new Password(EXAMPLE_PASSWORD);
+    passWrong = new Password(EXAMPLE_PASSWORD_WRONG);
+    passOK = new Password(EXAMPLE_PASSWORD_OK);
   });
 
   describe('Create Password', () =>{
     it('should create an instance of Password', ()=> {
-      const instante = new Password(EXAMPLE_PASSWORD);
+      const instante = new Password(EXAMPLE_PASSWORD_OK);
       expect(instante).toBeInstanceOf(Password);
     });
   })
   
   describe('Email Validations', () => {
     it('should be valid email address', () => {
-      expect(pass.validate()).toBeTruthy();
+      expect(passOK.validate()).toBeTruthy();
+    })
+    
+    it('should be valid email address', () => {
+      expect(passWrong.validate()).toBeFalsy();
     })
   })
   
