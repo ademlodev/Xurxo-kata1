31ca06d9646f678779291cce81b819270bcf701d
Javier García <javier.garcia.olmeda@gmail.com>
2023-11-14 16:48:40 +0100

    comments by xurxo

--- src/core/domain/email.ts
+++ src/core/domain/email.ts
@@ -3,14 +3,15 @@ export class Email {
     value: string
 
     constructor(email: string) {
+        this.validate(email); //TODO exception o either
         this.value = email
     }
 
-    public validate(): boolean {
+    private validate(email: string): boolean {
         const regexp = new RegExp(
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         )
 
-        return regexp.test(this.value)
+        return regexp.test(email)
     }
 }
